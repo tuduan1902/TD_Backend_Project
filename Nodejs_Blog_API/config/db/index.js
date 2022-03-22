f13d7180ae1fbe7b20db2386c05aca978440a0ba
tuduan1902 <uuxinhxinh2701@gmail.com>
2022-03-23 00:10:46 +0700

    commit

--- Nodejs_Blog_API/config/db/index.js
+++ Nodejs_Blog_API/config/db/index.js
@@ -5,6 +5,7 @@ async function connect(){
         await mongoose.connect('mongodb://localhost:27017/Blog',{
             useNewUrlParser:true,
             useUnifiedTopology: true,
+            useCreateIndex: true,
         });
         console.log('Connect sucessfully');
     } catch(error){
