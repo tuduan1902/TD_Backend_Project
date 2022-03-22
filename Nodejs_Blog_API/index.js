e487f127e199bfc5247e77580c46e4f865349939
tuduan1902 <uuxinhxinh2701@gmail.com>
2022-03-23 00:07:36 +0700

    commit

--- Nodejs_Blog_API/index.js
+++ Nodejs_Blog_API/index.js
@@ -1,2 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const db = require('./config/db');
+// Connect to DB
+db.connect();
+app.listen("5000",() =>{
+    console.log("Backend is running.");
+})
